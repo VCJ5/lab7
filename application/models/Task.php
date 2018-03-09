c703c13cd8d21db2d4b2ee18c3b63102e0ac89af
ianianda <ywang402@my.bcit.ca>
2018-03-08 16:16:06 -0800

    add task.php
    Signed-off-by: ianianda <ywang402@my.bcit.ca>

---
+++ application/models/Task.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Implements magic setter methods for each property of the Tasks model 
+ */
+require_once APPPATH . 'core/Entity.php';
+
+class Task extends Entity {
+    /**
+     * Evaluates and sets the Task property
+     */
+    private $task;
+    private $priority;
+    private $size;
+    private $group;
+    
+    public function setTask($value)
+    {
+        if (!ctype_alnum(trim(str_replace(' ','',$value)))) {
+            throw new Exception('does not consist of alpha, numeric and spaces');
+        }
+        if (strlen($value) > 10) {
+            throw new Exception('task name is too long');
+        }
+        $this->task = $value;
+    }
+
+    public function setPriority($value) 
+    {
+        if (!is_int($value)) {
+            throw new Exception('Must be an integer');
+        }
+        if ($value < 0 || $value > 9) {
+            throw new Exception('Must be between 0 to 9');
+        }
+        $this->priority = $value;
+    }
+    
+    public function setSize($value) 
+    {
+        if (!is_int($value)) {
+            throw new Exception('Must be an integer');
+        }
+        if ($value < 0 || $value > 20) {
+            throw new Exception('Must be between 0 to 20');
+        }
+        $this->size = $value;
+    }
+    
+    public function setGroup($value)
+    {
+        if (!is_int($value)) {
+            throw new Exception('Must be an integer');
+        }
+        if ($value < 1 || $value > 5) {
+            throw new Exception('Must be between 1 to 5');
+        }
+        $this->group = $value;
+    }
+    
+    public function getTask() {
+        return $this->task;
+    }
+    
+    public function getPriority() {
+        return $this->priority;
+    }
+    
+    public function getSize() {
+        return $this->size;
+    }
+    
+    public function getGroup() {
+        return $this->group;
+    }
+} 
